a7e44714a29e64097f92ad7eb1eceb8479c732e4
Mateusz Abramczyk <mateusz.abramczyk95@wp.pl>
2020-07-25 12:16:38 +0200

    editing cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,6 @@
 class CardsController < ApplicationController
+  before_action :set_user, only: %i[index new create update edit destroy show]
+
   def new
   end
 
@@ -18,5 +20,15 @@ class CardsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:type, :number, :expire_date, :ccv, :bank_name, :card_holder)
   end
 end
