cb2c4e5e80d75270288234fb7b7ea5bf9d04ca59
Sorina Duca <sorina.duca@gmail.com>
2020-07-25 15:21:30 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_101955) do
+ActiveRecord::Schema.define(version: 2020_07_25_131202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,10 @@ ActiveRecord::Schema.define(version: 2020_07_25_101955) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.text "weekday_opening_times"
+    t.text "saturday_opening_times"
+    t.text "sunday_opening_times"
   end
 
   add_foreign_key "bills", "users"
