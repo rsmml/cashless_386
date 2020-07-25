a10a87fdf3e7b94b70872e00f455de892949ad6e
Mateusz Abramczyk <mateusz.abramczyk95@wp.pl>
2020-07-25 12:59:52 +0200

    added card_policy and edited cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,26 +1,52 @@
 class CardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_card, only: %i[edit update show destroy]
+
+  def index
+    @cards = policy_scope(Card)
+  end
+
   def new
+    @card = Card.new
+    authorize @card
   end
 
   def create
-  end
-
-  def update
+    @card = Card.new(card_params)
+    @card.user = current_user
+    authorize @card
+    if @card.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def destroy
+  def update
+    if @card.update(card_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
-  def index
+  def destroy
+    @card.destroy
+    redirect_to root_path
   end
 
   def show
   end
 
   private
+
+  def find_card
+    @card = Card.find(params[:id])
+    authorize @card
+  end
 
   def card_params
     params.require(:card).permit(:type, :number, :expire_date, :ccv, :bank_name, :card_holder)
---
+++ app/policies/card_policy.rb
@@ -0,0 +1,22 @@
+class CardPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def show?
+    record.user == user
+  end
+
+  def create?
+    record.user == user
+  end
+
+  def update?
+    user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
