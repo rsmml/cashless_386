4402734a79208fb0a6d97e641f2cfa127d9bb6d9
Mateusz Abramczyk <mateusz.abramczyk95@wp.pl>
2020-07-25 11:45:27 +0200

    added card controller with empty crud actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+  get 'cards/create'
+  get 'cards/update'
+  get 'cards/edit'
+  get 'cards/destroy'
+  get 'cards/index'
+  get 'cards/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
