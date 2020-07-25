7dae07e2a896145f6f419f5079ee912cdc4cb59e
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-25 17:29:48 +0200

    create bill new page

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,6 @@
 class BillsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :create ]
+  skip_before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :authenticate_user!, only: [ :edit, :update ]
 
   def new
     @bill = Bill.new
@@ -7,7 +8,12 @@ class BillsController < ApplicationController
   end
 
   def create
-    raise
+    @bill = Bill.new(bill_params)
+    @bill.vendor = Vendor.find_by(params[:vendor_id])
+    @bill.user = User.find_by(params[:user_id])
+    @bill.price_cents = params[:price_cents]
+    @bill.save
+
   end
 
   def show
@@ -20,6 +26,12 @@ class BillsController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def bill_params
+    params.require(:bill).permit(:date, :price_cents, :status, :vendor_id, :user_id, )
   end
 
 end
