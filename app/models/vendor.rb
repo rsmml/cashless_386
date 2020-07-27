408b2e821439433249d0ba04ca82acd19e058b32
Sorina Duca <sorina.duca@gmail.com>
2020-07-27 12:48:17 +0200

    put rating on map vendor popup

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,5 @@
 class Vendor < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
