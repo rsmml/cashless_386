5ebc292390fc23af1cc62f5f53b4b27c6fa3933f
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-28 21:53:13 +0200

    config set user for 1 year

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:sessions => 'sessions'}
 
   resources :user, only: :show do
     resources :cards, only: :index
