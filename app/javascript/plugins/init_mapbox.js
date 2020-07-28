04d99d5136af2e2631a016f306110c9858f4078b
Sorina Duca <sorina.duca@gmail.com>
2020-07-28 11:24:08 +0200

    add track my location to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -28,6 +28,14 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
+    map.addControl(
+          new mapboxgl.GeolocateControl({
+          positionOptions: {
+          enableHighAccuracy: true
+          },
+          trackUserLocation: true
+          })
+        );
   }
 };
 
