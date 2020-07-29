e0477fab8284021361b6dc15f75fa79d63e4425e
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-29 19:01:14 +0200

    a lot of conclics fixed - initCheckuout function comment out from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,15 +30,15 @@ import "@stripe/stripe-js";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initPlusMenuDashboard } from '../components/plus-menu';
-import { initCheckout } from '../plugins/init_checkout';
+// import { initCheckout } from '../plugins/init_checkout';
+import { initStarRating } from '../plugins/init_star_rating';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
   initPlusMenuDashboard();
+  initStarRating();
 });
 
-import { initStarRating } from '../plugins/init_star_rating';
 
-initStarRating();
