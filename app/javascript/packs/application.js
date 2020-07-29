ec24e4eb1f1ef79cf94a350c40ba63912feade2d
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-29 17:13:42 +0200

    plus btn added with JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,11 @@ import "@stripe/stripe-js";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initPlusMenuDashboard } from '../components/plus-menu';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initPlusMenuDashboard();
 });
