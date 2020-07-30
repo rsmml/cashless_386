5a6faabc1358a05a071141aea78a50433d40b5a3
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-30 21:12:40 +0200

    delete unnecessary comma

--- app/controllers/bills/qrcodes_controller.rb
+++ app/controllers/bills/qrcodes_controller.rb
@@ -8,7 +8,7 @@ class Bills::QrcodesController < ApplicationController
         offset: 0,
         color: '000',
         shape_rendering: 'crispEdges',
-        module_size: 6,
+        module_size: 6
       }
 
     @text = bill_path(@bill)
