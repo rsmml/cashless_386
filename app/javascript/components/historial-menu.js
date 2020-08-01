8e499b674811c9b48399507e35e1768d0c559d91
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-08-01 11:44:21 +0200

    map-card in dashboard now gows up and down

--- app/javascript/components/historial-menu.js
+++ app/javascript/components/historial-menu.js
@@ -3,15 +3,29 @@ const initHistorialMenuUp = () => {
   if (history) {
     history.addEventListener('click',function(){
       document.querySelector('.last-movements-container').style.height = "90vh";
-      document.querySelector('.fa-chevron-up').style.display = "none";
-      document.querySelector('.fa-times').style.display = "flex";
-      document.querySelector('.fa-times').style.rotate = "540deg";
+      document.querySelector('.btn-up-card').style.display = "none";
+      document.querySelector('.btn-down-card').style.display = "flex";
+      document.querySelector('.btn-down-card').style.rotate = "540deg";
     });
 
     document.querySelector('.fa-times').addEventListener('click',function(){
       document.querySelector('.last-movements-container').style.height = "36vh";
-      document.querySelector('.fa-chevron-up').style.display = "flex";
-      document.querySelector('.fa-times').style.display = "none";
+      document.querySelector('.btn-up-card').style.display = "flex";
+      document.querySelector('.btn-down-card').style.display = "none";
+    });
+  };
+  const mapUp = document.querySelector('#btn-up-map')
+  if (mapUp) {
+    mapUp.addEventListener('click', function(){
+      document.querySelector('.db-map-container').style.height = "90vh";
+      document.querySelector('.btn-up-map').style.display = "none";
+      document.querySelector('.btn-close-map').style.display = "flex";
+      document.querySelector('.btn-close-map').style.rotate = "540deg";
+    });
+    document.querySelector('.btn-close-map').addEventListener('click',function(){
+      document.querySelector('.db-map-container').style.height = "36vh";
+      document.querySelector('.btn-up-map').style.display = "flex";
+      document.querySelector('.btn-close-map').style.display = "none";
     });
   };
 };
