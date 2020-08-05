acc95bd87648319acf2c750cbfd18c8b7f4d5a8f
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-08-05 14:29:56 +0200

    set up camera

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :dashboards, only: %i[show map]
 
   get 'camera', to: 'dashboards#camera', as: 'camera'
+  # resources :qr_codes, only: [:new, :create, :show]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
