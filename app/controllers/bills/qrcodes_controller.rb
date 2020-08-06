24971a96fec9a3c5bfb83ec9812bb0c6d962ed76
Chihoko Ueno <harrierraum@hotmail.com>
2020-08-06 17:26:07 +0200

    change the link of QR code to heroku

--- app/controllers/bills/qrcodes_controller.rb
+++ app/controllers/bills/qrcodes_controller.rb
@@ -11,8 +11,8 @@ class Bills::QrcodesController < ApplicationController
         module_size: 6,
       }
 
-    @text = "http://www.cashless-386.xyz#{bill_path(@bill)}"
-    # @text = bill_path(@bill)
+    @text = "http://www.cashless-386.xyz#{new_bill_payment_path(@bill)}"
+    # @text = new_bill_payment_path(@bill)
     @qrcode = RQRCode::QRCode.new(@text)
     @svg = @qrcode.as_svg(options)
   end
