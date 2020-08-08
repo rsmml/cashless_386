d07b77fe045ee7e49976c5e1220f63a771f8f5fd
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-08-08 11:32:23 +0200

    seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -458,35 +458,3 @@ puts "Creating a bill"
     price_cents: 2789,
     )
 puts "Bill #{bill_first.id} was created"
-
-puts "Seeds Done"
-
-
-
-
-
-Bill.create(
-    date: "2020-08-01",
-    status: "pending",
-    user_id: 18,
-    vendor_id: 26,
-    price_cents: 2789,
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
