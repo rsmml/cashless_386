e5386b15fd65d66011feb8af198ac74bcd270fdb
Chihoko Ueno <harrierraum@hotmail.com>
2020-08-10 18:44:01 +0200

    add charge column to bills table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_163918) do
+ActiveRecord::Schema.define(version: 2020_08_10_164208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_163918) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
     t.string "qrcode"
+    t.string "charge"
     t.index ["user_id"], name: "index_bills_on_user_id"
     t.index ["vendor_id"], name: "index_bills_on_vendor_id"
   end
