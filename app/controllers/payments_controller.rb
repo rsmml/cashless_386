fd1ef198dc7821420045b11123db03cee7426448
Chihoko Ueno <harrierraum@hotmail.com>
2020-08-10 21:08:52 +0200

    delete unnecessary line

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,5 @@
 class PaymentsController < ApplicationController
   protect_from_forgery
-  # skip_before_action :verify_authenticity_token, only: :create
   before_action :set_bill, only: [:new, :create]
 
   def new
