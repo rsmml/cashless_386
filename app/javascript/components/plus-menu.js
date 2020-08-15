80c015eca221d098a8f74cc721f74593cf3fffa2
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-08-15 15:59:22 +0200

    change btn camera dashboard to opacity 1

--- app/javascript/components/plus-menu.js
+++ app/javascript/components/plus-menu.js
@@ -2,14 +2,7 @@ const initPlusMenuDashboard = () => {
   const plusMenu = document.querySelector('#plus-menu')
   if (plusMenu) {
     plusMenu.addEventListener('click',
-    function(){
-      document.querySelector('.plus-menu').style.opacity = "1";
-      setTimeout(
-        function() {
-          document.querySelector('.plus-menu').style.opacity = "0.5";
-          window.location.href = '/camera';
-        }, 500);
-    });
+      window.location.href = '/camera');
   };
 };
 
