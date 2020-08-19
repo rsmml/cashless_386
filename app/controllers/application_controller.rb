b8f480a40e9b670a53a2cb142c242bf31797e1bc
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-08-19 11:22:19 +0200

    include MetaTagsHelper added to applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
+  include MetaTagsHelper
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
